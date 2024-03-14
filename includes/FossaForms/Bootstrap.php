d99827175b15f1ec4da18244e1caadc6dbbe14f4
jossafossa <joosthobma@gmail.com>
2024-03-14 00:18:56 -0300

    init

---
+++ includes/FossaForms/Bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+namespace FossaForms;
+
+class Bootstrap
+{
+
+    public static function init($henk = "") {
+        $test = ['hoi' => "asdasd"];
+        print( $test[ 'hoi' ]);
+
+        if (true) {
+            echo "true";
+        }
+
+        if (false) {
+            echo "false";
+        }
+
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use FossaForms\Bootstrap;
+
+// Initialize the plugin.
+Bootstrap::init();
